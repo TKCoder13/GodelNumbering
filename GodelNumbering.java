fe5fda89f9e7022c864fea200c37f17da20afcc5
TKCoder13 <TKCoding13@gmail.com>
2023-01-23 18:38:42 -0800

    I am a god

--- GodelNumbering.java
+++ GodelNumbering.java
@@ -6,7 +6,7 @@ public class GodelNumbering
 {
     public static void main(String args[]) 
     {
-        int[] input = {2,2,2,3,1,2,3,3,1,1,1,1,1,11};
+        int[] input = {7,6,5,4,3,2,1};
         int len = input.length;
         int[] primeNums = new int[len];
         int testNum = 0;
@@ -14,13 +14,50 @@ public class GodelNumbering
         {
             primeNums[i] = Prime.nextPrime(testNum);
             testNum = primeNums[i];
-            System.out.println(primeNums[i]);
+            System.out.println("This is prime #" + (i+1) + ": " + primeNums[i]);
         }
-        // long e = 1;
-        // for (int i = 0; i < len; i++)
-        // {
-        //     e *= (long)(Math.pow(primeNums[i], input[i]));
-        //     System.out.println(e);
-        // }
+        System.out.println(); 
+        long e = 1;
+        for (int i = 0; i < len; i++)
+        {
+            e *= (long)(Math.pow(primeNums[i], input[i]));
+            System.out.println("e = " + e + " at primeNums index " + i);
+        }
+        System.out.println(); //skips a line
+        //Everything above is for the input to get to e.
+        //Everything below is for e to get to the input.
+        // int[] tempPrimeFactors = Prime.primeFactors(e);
+        int[] eOutput = Prime.primeFactors(e);
+        int[] finalOutput = new int[len];
+
+        System.out.print("{");
+        int index = 0;
+        for (int i = 0; i < eOutput.length - 1; i++)
+        {
+            System.out.print(eOutput[i] + ",");
+        }
+        System.out.println(eOutput[eOutput.length - 1] + "}");
+        System.out.println();
+        for (int i = 0; i < eOutput.length; i++)
+        {
+            if (primeNums[index] == eOutput[i])
+            {
+                finalOutput[index]++;
+            } else
+            {
+                index++;
+                if (primeNums[index] == eOutput[i])
+                {
+                    finalOutput[index]++;
+                }
+            }
+        }
+        System.out.println("The Final Product is: ");
+        System.out.print("{");
+        for (int i = 0; i < finalOutput.length - 1; i++)
+        {
+            System.out.print(finalOutput[i] + ",");
+        }
+        System.out.print(finalOutput[finalOutput.length - 1] + "}");
     }
 }
--- Prime.java
+++ Prime.java
@@ -24,31 +24,23 @@ public class Prime
         return true;
     }
 
-    public int[] primeFactors(int p) //this will be for reverting e back to prime numbers
-    {
-        int[] primeFactors = new int[p];
-        int index = 0;
-        primeFactors[index++] = 1;
-        if (isPrime(p)) 
+    public static boolean isPrime(long n) throws IllegalArgumentException {
+        long one = 1;
+        long two = 2;
+        if (n == one) 
         {
-            primeFactors[1] = p;
-            return primeFactors;
-        } else 
+            return false;
+        } else if (n == two)
         {
-            for (int i = 2; i <= p; i++) 
-            {
-                if (p % i == 0) 
-                {
-                    primeFactors[index++] = i;
-                }
+            return true;
+        }
+        for (long i = 2; i < n; i++) 
+        {
+            if (n % i == 0) { //check for divisors other than 1 and p
+                return false;
             }
         }
-        int[] output = new int[index];
-        for (int i = 0; i < index; i++) 
-        {
-            output[i] = primeFactors[i];
-        }
-        return output;
+        return true;
     }
 
     public static int nextPrime(int num) 
@@ -72,7 +64,79 @@ public class Prime
             in = 2;
             return in;
         }
-        
-        
+    }   
+
+    // public int[] primeFactors(long e) //this will be for reverting e back to prime numbers
+    // {
+    //     long p = e;
+    //     int[] primeFactors = new int[100];
+    //     int index = 0;
+    //     primeFactors[index++] = 1;
+    //     if (p == 1) {
+    //         int[] one = new int[1];
+    //         one[0] = 1;
+    //         return one;
+    //         System.out.println("This is not a ")
+    //     } else if (isPrime(p)) 
+    //     {
+    //         primeFactors[1] = p;
+    //         return primeFactors;
+    //     } else 
+    //     {
+    //         for (int i = 2; i <= p; i++) 
+    //         {
+    //             if (p % i == 0) 
+    //             {
+    //                 primeFactors[index++] = i;
+    //             }
+    //         }
+    //     }
+    //     int[] output = new int[index];
+    //     for (int i = 0; i < index; i++) 
+    //     {
+    //         output[i] = primeFactors[i];
+    //     }
+    //     return output;
+    // }
+
+    public static int[] primeFactors(long e)
+    {
+        long input = e;
+        int primeTester = Prime.nextPrime(0);
+        int[] listOfFactors = new int[100];
+        int endCount = 0;
+        long half = input / 2;
+        int temp = 0;
+        if (input % 2 == 0)
+        {
+            for (long i = 0; i <= input / 2; i++)
+            {
+                while (input % primeTester == 0)
+                {
+                    input /= primeTester;
+                    listOfFactors[temp++] = primeTester;
+                    endCount++;
+                }
+                primeTester = Prime.nextPrime(primeTester);
+            }
+        } else 
+        {
+            for (long i = 0; i <= (input + 1) / 2; i++)
+            {
+                while (input % primeTester == 0)
+                {
+                    input /= primeTester;
+                    listOfFactors[temp++] = primeTester;
+                    endCount++;
+                }
+                primeTester = Prime.nextPrime(primeTester);
+            }
+        }
+        int[] output = new int[endCount];
+        for (int i = 0; i < endCount; i++)
+        {
+            output[i] = listOfFactors[i];
+        }
+        return output;
     }
 }
